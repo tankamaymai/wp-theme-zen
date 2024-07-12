0b0db02ba113abeb0eb087b479efafbab33d4fc6
tokoton-yoshizumi <info@tokoton-design.com>
2024-07-12 16:45:47 +0900

    change

--- inc/customizer/main-visual.php
+++ inc/customizer/main-visual.php
@@ -56,13 +56,102 @@ function mytheme_customize_main_visual($wp_customize)
         $wp_customize->add_setting("mytheme_slider_bodycopy_$i", array(
             'default'   => '',
             'transport' => 'refresh',
-            'sanitize_callback' => 'sanitize_textarea_field',
+            'sanitize_callback' => 'sanitize_text_field',
         ));
         $wp_customize->add_control("mytheme_slider_bodycopy_$i", array(
             'label'    => __("ボディコピー $i", 'mytheme'),
             'section'  => 'mytheme_slider_settings',
             'type'     => 'textarea',
         ));
+
+        // テキストの位置設定
+        $wp_customize->add_setting("mytheme_slider_catchphrase_position_$i", array(
+            'default'   => 'center',
+            'transport' => 'refresh',
+            'sanitize_callback' => 'sanitize_text_field',
+        ));
+        $wp_customize->add_control("mytheme_slider_catchphrase_position_$i", array(
+            'label'    => __("テキストの位置 $i", 'mytheme'),
+            'section'  => 'mytheme_slider_settings',
+            'type'     => 'select',
+            'choices'  => array(
+                'left'   => __('左', 'mytheme'),
+                'center' => __('中央', 'mytheme'),
+                'right'  => __('右', 'mytheme'),
+            ),
+        ));
+
+
+        // キャッチコピーの文字サイズ設定
+        $wp_customize->add_setting("mytheme_slider_catchphrase_font_size_$i", array(
+            'default'   => 'x-large',
+            'transport' => 'refresh',
+            'sanitize_callback' => 'sanitize_text_field',
+        ));
+        $wp_customize->add_control("mytheme_slider_catchphrase_font_size_$i", array(
+            'label'    => __("キャッチコピーの文字サイズ $i", 'mytheme'),
+            'section'  => 'mytheme_slider_settings',
+            'type'     => 'select',
+            'choices'  => array(
+                'small'  => __('小', 'mytheme'),
+                'medium' => __('中', 'mytheme'),
+                'large'  => __('大', 'mytheme'),
+                'x-large' => __('特大', 'mytheme'),
+            ),
+        ));
+
+        // ボディコピーの文字サイズ設定
+        $wp_customize->add_setting("mytheme_slider_bodycopy_font_size_$i", array(
+            'default'   => 'medium',
+            'transport' => 'refresh',
+            'sanitize_callback' => 'sanitize_text_field',
+        ));
+        $wp_customize->add_control("mytheme_slider_bodycopy_font_size_$i", array(
+            'label'    => __("ボディコピーの文字サイズ $i", 'mytheme'),
+            'section'  => 'mytheme_slider_settings',
+            'type'     => 'select',
+            'choices'  => array(
+                'small'  => __('小', 'mytheme'),
+                'medium' => __('中', 'mytheme'),
+                'large'  => __('大', 'mytheme'),
+            ),
+        ));
+
+        // キャッチコピーの文字色設定
+        $wp_customize->add_setting("mytheme_slider_catchphrase_color_$i", array(
+            'default'   => '#ffffff',
+            'transport' => 'refresh',
+            'sanitize_callback' => 'sanitize_hex_color',
+        ));
+        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "mytheme_slider_catchphrase_color_$i", array(
+            'label'    => __("キャッチコピーの文字色 $i", 'mytheme'),
+            'section'  => 'mytheme_slider_settings',
+            'settings' => "mytheme_slider_catchphrase_color_$i",
+        )));
+
+        // ボディコピーの文字色設定
+        $wp_customize->add_setting("mytheme_slider_bodycopy_color_$i", array(
+            'default'   => '#ffffff',
+            'transport' => 'refresh',
+            'sanitize_callback' => 'sanitize_hex_color',
+        ));
+        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "mytheme_slider_bodycopy_color_$i", array(
+            'label'    => __("ボディコピーの文字色 $i", 'mytheme'),
+            'section'  => 'mytheme_slider_settings',
+            'settings' => "mytheme_slider_bodycopy_color_$i",
+        )));
+
+        // テキストの背景色設定
+        $wp_customize->add_setting("mytheme_slider_text_background_color_$i", array(
+            'default'   => '#333333',
+            'transport' => 'refresh',
+            'sanitize_callback' => 'sanitize_text_field',
+        ));
+        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "mytheme_slider_text_background_color_$i", array(
+            'label'    => __("テキストの背景色 $i", 'mytheme'),
+            'section'  => 'mytheme_slider_settings',
+            'settings' => "mytheme_slider_text_background_color_$i",
+        )));
     }
 }
 
@@ -85,14 +174,55 @@ function mytheme_display_swiper()
         'https://wpzen.jp/assets/images/sample05.jpg',
     );
 
+    // フォントサイズのクラスを取得
+    function get_font_size_class($size)
+    {
+        switch ($size) {
+            case 'small':
+                return 'font-size-small';
+            case 'medium':
+                return 'font-size-medium';
+            case 'large':
+                return 'font-size-large';
+            case 'x-large':
+                return 'font-size-x-large';
+            default:
+                return 'font-size-medium';
+        }
+    }
+
+
 ?>
     <div class="swiper swiper-container">
         <div class="swiper-wrapper">
             <?php for ($i = 1; $i <= 5; $i++) : ?>
-                <?php $img_url = get_theme_mod("mytheme_slider_image_$i", $default_images[$i - 1]);
+                <?php
+                $img_url = get_theme_mod("mytheme_slider_image_$i", $default_images[$i - 1]);
+                $catchphrase = get_theme_mod("mytheme_slider_catchphrase_$i", '');
+                $bodycopy = get_theme_mod("mytheme_slider_bodycopy_$i", '');
+                $position = get_theme_mod("mytheme_slider_catchphrase_position_$i", 'center');
+                $catchphrase_font_size = get_theme_mod("mytheme_slider_catchphrase_font_size_$i", 'x-large');
+                $bodycopy_font_size = get_theme_mod("mytheme_slider_bodycopy_font_size_$i", 'medium');
+                $catchphrase_color = get_theme_mod("mytheme_slider_catchphrase_color_$i", '#ffffff');
+                $bodycopy_color = get_theme_mod("mytheme_slider_bodycopy_color_$i", '#ffffff');
+                $text_background_color = get_theme_mod("mytheme_slider_text_background_color_$i", '#333333');
                 if ($img_url) : ?>
                     <div class="swiper-slide">
                         <img src="<?php echo esc_url($img_url); ?>" alt="Slide <?php echo $i; ?>" loading="lazy">
+                        <?php if ($catchphrase || $bodycopy) : ?>
+                            <div class="slider-text slider-text-<?php echo $position; ?>" style="background-color: <?php echo esc_attr($text_background_color); ?>;">
+                                <?php if ($catchphrase) : ?>
+                                    <h2 class="slider-catchphrase <?php echo get_font_size_class($catchphrase_font_size); ?>" style="color: <?php echo esc_attr($catchphrase_color); ?>;">
+                                        <?php echo esc_html($catchphrase); ?>
+                                    </h2>
+                                <?php endif; ?>
+                                <?php if ($bodycopy) : ?>
+                                    <p class="slider-bodycopy <?php echo get_font_size_class($bodycopy_font_size); ?>" style="color: <?php echo esc_attr($bodycopy_color); ?>;">
+                                        <?php echo esc_html($bodycopy); ?>
+                                    </p>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 <?php endif; ?>
             <?php endfor; ?>
